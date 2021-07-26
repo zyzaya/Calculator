cf6ebe3a589f00e266e44f1a06f8735c9ab25846
Riley Sherlock <riley.sherlock@mts.net>
2021-07-26 17:11:41 -0500

    Change display to contain more information
    The calculator display now shows the operator in addition
    to operands. Additionaly it displays 0 by default instead
    of empty spaces.

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-// keyboard, visuals, history display, 
-// display defaults to zero instead of nothing
+// visuals
 
 let first_operand = null;
 let second_operand = null;
 let operator = null;
 
 let output = document.getElementById('output');
-output.textContent = "";
+output.textContent = "0";
 
 function addDecimal(id) {
-    if (!output.innerText.includes('.')) {
+    if ((first_operand === null && !output.innerText.includes('.')
+        || (operator !== null && !output.innerText.split(operator)[1].includes('.')))) {
         setDisplay(id);
     }
 }
 
 function setDisplay(id) {
+    if (output.innerText === '0' && id !== '.')
+        output.innerText = "";
     output.innerText = output.innerText + id;
 }
 
 function deleteLastInput() {
     output.innerText = output.innerText.slice(0, -1)
-    current_display = output.innerText;
+    if (output.innerText.length === 0)
+        output.innerText = '0';
 }
 
 function setOperator(id) {
     operator = id;
-    if (first_operand === null) {
+    if (first_operand === null) { 
         first_operand = parseFloat(output.textContent);
-        output.textContent = null;
+        setDisplay(operator)
     }
 }
 
 function clearEntry() {
-    output.textContent = null;
+    output.textContent = '0';
 }
 
 function clear() {
@@ -45,7 +48,9 @@ function clear() {
 function calculate() {
     if (operator && first_operand) {
         let result = null;
-        second_operand = parseFloat(output.textContent);
+        console.log(output.textContent.split(operator)[1])
+        second_operand = parseFloat(output.textContent.split(operator)[1]);
+        console.log(second_operand)
         switch (operator) {
             case '/':
                 result = first_operand / second_operand;
